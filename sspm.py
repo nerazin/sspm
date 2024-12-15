87529fc24839babd7a98fa226209ebc4b38da5c8
Stepan Simakov <stsi@juicyscore.com>
2024-12-15 16:50:41 +0300

    better handling + mimetypes fix

--- sspm.py
+++ sspm.py
@@ -1,7 +1,10 @@
 from http.server import BaseHTTPRequestHandler
 from http.server import HTTPServer
 from random import randint
+import mimetypes
 import os
+import sspm_config
+from functools import cached_property
 
 
 class HttpGetHandler(BaseHTTPRequestHandler):
@@ -23,29 +26,33 @@ class HttpGetHandler(BaseHTTPRequestHandler):
         filename, extension = os.path.splitext(sys_path)
         with open(sys_path, 'rb') as file_to_send:
             if extension in ('.ico', '.jpg', '.png'):
-                data_from_file = (file_to_send.read(), 'img')
+                data_from_file = (file_to_send.read(), 'file', extension)
             elif extension in ('.html', '.js', '.css', '.json', '.txt'):
-                data_from_file = (file_to_send.read(), 'text')
+                data_from_file = (file_to_send.read(), 'text', extension)
             return data_from_file
 
 
     def do_GET(self):
         data_to_send = self.read_file_structure(self.path)
         if data_to_send:
-            if data_to_send[1] == 'img':
+            file_mimetype = sspm_config.sspm_mimetypes[data_to_send[2]]
+            if data_to_send[1] == 'file':
                 self.send_response(200)
-                self.send_header("Content-Type", "image/x-icon")
+                self.send_header("Content-Type", file_mimetype)
                 self.send_header("Content-Length", len(data_to_send[0]))
                 self.end_headers()
                 self.wfile.write(data_to_send[0])
             elif data_to_send[1] == 'text':
                 data_to_send = self.simple_template_engine(self.path, data_to_send[0].decode())
                 self.send_response(200)
-                self.send_header("Content-type", "text/html")
+                self.send_header("Content-type", file_mimetype)
                 self.end_headers()
                 self.wfile.write(data_to_send.encode())
         else:
             self.send_response(404)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+            self.wfile.write(b'<h1>404</h1>')
 
 
 def run(server_class=HTTPServer, handler_class=HttpGetHandler):
---
+++ sspm_config.py
@@ -0,0 +1,15 @@
+sspm_mimetypes = {
+	# text
+	'.js': 'text/javascript',
+	'.html': 'text/html',
+	'.css': 'text/css',
+	'.json': 'application/json',
+	# img
+	'.ico': 'image/x-icon',
+	'.png': 'image/png',
+	'.jpg': 'image/jpeg',
+	'.svg': 'image/svg+xml',
+	'.tiff': 'image/tiff',
+	'.tif': 'image/tiff',
+	'.webp': 'image/webp',
+}
